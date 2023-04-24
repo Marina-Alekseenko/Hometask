acb3715d00f65abd8bdb8ce8299b806387ef9213
marina-alekseenko <alekseenko.ads@gmail.com>
2023-04-24 12:54:12 +0300

    task#8 solved

---
+++ 24-04--task-8/task-8.cs
@@ -0,0 +1,20 @@
+﻿//  Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+string n = Console.ReadLine();
+int n1 = Convert.ToInt32(n);
+int X = 2;
+if (n1 >= X)
+{
+    while (n1 >= X) 
+    {
+    Console.WriteLine(X);
+    X = X + 2;
+    }
+}
+else
+{
+    Console.WriteLine("Введены некорректные данные");
+}
